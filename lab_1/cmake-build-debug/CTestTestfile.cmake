c808b7726d48f8fa6e37b07d4a50d6bd57b35c94
matv3yka <116829245+matv3ykaa@users.noreply.github.com>
2025-04-17 00:50:37 +0300

    hotfix #2

--- lab_1/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/matveyd/CLionProjects/OOP/Lab1
-# Build directory: /Users/matveyd/CLionProjects/OOP/Lab1/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googletest")
